4afa43c4e96925ed63100bba9ac267beaee43989
jameslatimerr <james.latimer@ucf.edu>
2022-05-31 23:34:30 -0400

    setup for backend calls

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
   data() {
     return {
@@ -18,7 +18,16 @@ export default {
   },
   methods : {
     SetMessage: function() {
-      this.msg = 'yo';
+     const path = 'http://localhost:5000/ping';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+
     }
   }
 }
