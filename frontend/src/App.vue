5957d117af508b885f0240ef625bc01831c8b099
jameslatimerr <james.latimer@ucf.edu>
2022-05-31 23:17:23 -0400

    converted submodule to regular file

---
+++ frontend/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="nav">
+    <h1> {{this.msg}} </h1>
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+    <button @click="SetMessage"> Set message</button>
+  </div>
+  <!-- <router-view/> -->
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      msg:  '0',
+    }
+  },
+  methods : {
+    SetMessage: function() {
+      this.msg = 'yo';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
